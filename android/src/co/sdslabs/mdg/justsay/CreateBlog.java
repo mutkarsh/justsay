cbad3f6690ef7efdf39e4ddfec2d44e6442324c1
utkarsh <shanu.um123@gmail.com>
2014-02-27 04:19:54 +0530

    send data to url

--- android/src/co/sdslabs/mdg/justsay/CreateBlog.java
+++ android/src/co/sdslabs/mdg/justsay/CreateBlog.java
@@ -1,5 +1,10 @@
 package co.sdslabs.mdg.justsay;
 
+import java.util.ArrayList;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +16,7 @@ public class CreateBlog extends Activity implements OnClickListener{
 
 	Button bSubmit;
 	EditText etTitle,etDescription,etTags;
+	final String Title="Title";
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -21,14 +27,27 @@ public class CreateBlog extends Activity implements OnClickListener{
 		etDescription=(EditText)findViewById(R.id.etDescription);
 		etTags=(EditText)findViewById(R.id.etTags);
 		etTitle=(EditText)findViewById(R.id.etTitle);
-		
+		//setting onClickListener
 		bSubmit.setOnClickListener(this);		
 	}
+	//
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		
+		//###################################
+		
+		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+
+		nameValuePairs.add(new BasicNameValuePair("Title",etTitle.getText().toString()));
+		nameValuePairs.add(new BasicNameValuePair("Description",etDescription.getText().toString()));
+		nameValuePairs.add(new BasicNameValuePair("Tags",etTags.getText().toString()));
+		       // nameValuePairs.add(new BasicNameValuePair("location",gpslocation_str));
+		InternetData data=new InternetData();
+		String out=data.postData("http://bloggy.com/create_blogposts_backened.php", nameValuePairs);
+
+		        }
+		
 	}
 
-}
